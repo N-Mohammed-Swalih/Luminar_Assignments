4f8a13d1a003b3d1c1991b7e48ec613724a726a4
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-08-04 14:59:06 +0530

    commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -22,18 +21,26 @@ class _LoginPageState extends State<LoginPage> {
           key: formkey, // this key used to fetch the current state of the form
           child: Column(
             children: [
-              const SizedBox(height: 250,),
-              Text("Login",
-              style: GoogleFonts.roboto(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-              ),),
-              SizedBox(height: 10,),
+              const SizedBox(
+                height: 250,
+              ),
+              Text(
+                "Login",
+                style: GoogleFonts.roboto(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
               Center(
-                child: Text("Welcome Back! Login with your credentials",
-                style: TextStyle(
-                  color: Colors.grey,
-                ),),
+                child: Text(
+                  "Welcome Back! Login with your credentials",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 20.0, left: 10, right: 10),
@@ -41,7 +48,7 @@ class _LoginPageState extends State<LoginPage> {
                   decoration: InputDecoration(
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10)),
-                          prefixIcon: Icon(Icons.mail),
+                      prefixIcon: Icon(Icons.mail),
                       labelText: "Email ID",
                       hintText: "Enter your email id"),
                   validator: (username) {
@@ -66,19 +73,20 @@ class _LoginPageState extends State<LoginPage> {
                     }
                   },
                   decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.password),
-                      suffixIcon: IconButton(onPressed: (){
-                        setState(() {
-                        if(nopasswordvisibility == true)
-                        {
-                          nopasswordvisibility=false;
-                        }
-                        else{
-                          nopasswordvisibility=true;
-                        }
-                        });
-                      },
-                      icon:  Icon(nopasswordvisibility == true ? Icons.visibility_off_sharp : Icons.visibility) ),
+                      prefixIcon: Icon(Icons.password),
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              if (nopasswordvisibility == true) {
+                                nopasswordvisibility = false;
+                              } else {
+                                nopasswordvisibility = true;
+                              }
+                            });
+                          },
+                          icon: Icon(nopasswordvisibility == true
+                              ? Icons.visibility_off_sharp
+                              : Icons.visibility)),
                       hintText: "Enter your password",
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10)),
@@ -87,18 +95,16 @@ class _LoginPageState extends State<LoginPage> {
               ),
               Center(
                 child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    fixedSize: const Size(350, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30)
-                  )
-                  ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        fixedSize: const Size(350, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
                     onPressed: () {
                       final valid = formkey.currentState!.validate();
                       if (valid) {
-                        Navigator.of(context).push(
-                            MaterialPageRoute(builder: (context) => HomePage()));
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => HomePage()));
                       } else {
                         Fluttertoast.showToast(
                             msg: "Invalid Username or Password",
@@ -112,15 +118,19 @@ class _LoginPageState extends State<LoginPage> {
                     },
                     child: Text("Login")),
               ),
-              SizedBox(height: 10,),
+              SizedBox(
+                height: 10,
+              ),
               Center(
-                child: TextButton(onPressed: (){
-                  Navigator.of(context).push(
-                            MaterialPageRoute(builder: (context) => RegistrationPage()));
-                },child: Text("Do you have an account? Sign Up",
-                style: TextStyle(
-                  color: Colors.grey
-                ),)),
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => RegistrationPage()));
+                    },
+                    child: Text(
+                      "Do you have an account? Sign Up",
+                      style: TextStyle(color: Colors.grey),
+                    )),
               )
             ],
           ),
