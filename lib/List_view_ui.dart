6a8186215efc6b307b4b8326f8389f46f559c10b
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-08-06 12:22:12 +0530

    commit

--- lib/List_view_ui.dart
+++ lib/List_view_ui.dart
@@ -1,56 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp (MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:ListViewUi()));
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ListViewUi()));
 }
 
 class ListViewUi extends StatelessWidget {
-   ListViewUi ({super.key});
-  var images = [
-    "https://images.unsplash.com/photo-1606757389723-23c4bf501fba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  ListViewUi({super.key});
+  var image = [
+    "https://png.pngtree.com/png-vector/20210522/ourmid/pngtree-apple-is-naturally-thirsty-and-healthy-png-image_3323218.jpg",
     "https://images.unsplash.com/photo-1587735243615-c03f25aaff15?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80",
-
   ];
+
+  var description = ["Apple", "Orange"];
+
+  var price = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title : Center(child: Text("Product List"
-        ),),
-         actions: <Widget>[
-          SizedBox(height: 10,),
-    Badge(
-      backgroundColor: Colors.red,
-      alignment : Alignment.topRight,
-        label: Text('0'),
-      child: IconButton(
-        icon: Icon(
-          Icons.shopping_cart,
-          color: Colors.white,
-        ),
-        onPressed: () {
-          // do something
-        },
-      ),
-    ),
-    SizedBox(width: 30,),
-  ],
-),
-      body: ListView(
-        children: List.generate(2, (index) => Container(
-          height: 100,
-          child: Card(
-            child: ListTile(
-              leading: Image.network(images[index],),
-            ),
-        
+        appBar: AppBar(
+          title: Center(
+            child: Text("Product List"),
           ),
-        )),
-      ),
-
-    );
-}
+        )
+        );
+  }
 }
