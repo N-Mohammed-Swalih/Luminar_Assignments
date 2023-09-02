0ccd333346e46328e8331f5ff37e11d60c0c049a
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-09-02 12:20:54 +0530

    commit

--- lib/Farmers Fresh/farmer_fresh_home_page.dart
+++ lib/Farmers Fresh/farmer_fresh_home_page.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  var categoryname = [
+    'VEGETABLES',
+    'FRUITS',
+    'PACKED FOODS',
+    'FOOD GRAINS',
+    'BEVERAGES'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +23,18 @@ class HomePage extends StatelessWidget {
           floating: true,
           pinned: false,
           elevation: 0,
-          title: Text(
-            "Farmer Fresh Zone",
-            style: GoogleFonts.openSans(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Farmer Fresh Zone",
+                style: GoogleFonts.openSans(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              IconButton(onPressed: (){}, icon: Icon(Icons.location_on))
+            ],
           ),
           bottom: AppBar(
             toolbarHeight: 81,
@@ -32,11 +47,11 @@ class HomePage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 focusColor: Colors.white,
-                hintStyle: TextStyle(color: Colors.grey),
+                hintStyle: const TextStyle(color: Colors.grey),
                 hintText: "Search for Vegetables,Fruits...",
                 prefixIconColor: Colors.grey,
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                prefixIcon: const Padding(
+                  padding: EdgeInsets.all(8.0),
                   child: Icon(Icons.search_sharp),
                 ),
               ),
@@ -44,19 +59,38 @@ class HomePage extends StatelessWidget {
           ),
         ),
         SliverToBoxAdapter(
-            child: Container(
-                height: 100.0,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 10,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        width: 100.0,
-                        child: Card(
-                          child: Text('data'),
+            child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+              height: 50.0,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return SizedBox(
+                      width: 200.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            backgroundColor: Colors.green.shade100,
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            categoryname[index],
+                            style: TextStyle(
+                                color: Colors.green.shade900,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
-                      );
-                    })))
+                      ),
+                    );
+                  })),
+        ))
       ])),
     );
   }
