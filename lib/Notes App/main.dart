373d252059508136d89f2267ee26bbbc08c0f426
N-Mohammed-Swalih <mohdswalih003@gmail.com>
2023-09-28 18:53:36 +0530

    commit

--- lib/Notes App/main.dart
+++ lib/Notes App/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:hive_flutter/adapters.dart';
-
 import 'splash_screen.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   await Hive.initFlutter();
-  await Hive.openBox('to_do_app Box');
+  await Hive.openBox('Notes App Box');
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     home: SplashScreenNotesApp(),
